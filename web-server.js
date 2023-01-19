d25b291adcd6bbe831ef74e0f7aadcacffe45c2a
Ain Shaikh <ainsaikhx7@gmail.com>
2023-01-19 16:13:51 +0530

    :rocket:web-server and API-call

--- web-server.js
+++ web-server.js
@@ -1,10 +1,34 @@
-// Building a simple web server
+// Building a simple web server and API call with synchronous
 
-const http = require('http');
-const server = http.createServer((req,res)=>{
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
-    res.end("Hello from the server");
+const data = fs.readFileSync(
+  "../starter/dev-data/data.json",
+  "utf-8",
+  (err, data) => {
+    const dataObj = JSON.parse(data);
+  }
+);
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "Hello world",
+    });
+    res.end("<h4>Page not found</h4>");
+  }
 });
-server.listen(8000, '127.0.0.1', ()=>{
-    console.log("Listening to request on the port 8000");
-})
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on the port 8000");
+});
